2ff8b85b1f42af3890cfca5eff5f2939405f8922
crlfnsk <cafenski@gmail.com>
2024-11-18 20:25:55 +0100

    added some parameters and markdown format

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Nobody Hi Antonio and Caro! 
+# N-body System
+
+This readme file is supposed to explain how to use the nbody program.
+
+This program was designed by Caro, Antonio and Carl. 
--- src/parameters.f90
+++ src/parameters.f90
@@ -13,6 +13,11 @@ MODULE parameters
   INTEGER, PARAMETER :: SP  = KIND(1.0)
   INTEGER, PARAMETER :: DP  = KIND(1.d0)
 
-  REAL (KIND=8) :: solarmass = 8.1
+  REAL (KIND=dp), PARAMETER    :: PI = acos(-1.d0) 
+  ! Gravitational const. in m^3kg^-1s^-2, Astronomical unit in m
+  REAL (KIND=dp), PARAMETER    :: G = 6.673e-11, au = 1.49598e11
+  ! Solarradius in au, Solarmass in kg
+  REAL (KIND=dp), PARAMETER    :: Solarradius = 0.00464912633, Solarmass = 1.98892e30
+
   
 END MODULE parameters
